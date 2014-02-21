c7977ab42594ee75fff567db8e2eb8a166344cd1
Vlasov Dmitriy <scailer@veles.biz>
2014-02-21 16:28:36 +0600

    fix setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email='scailer@russia.ru',
 
     packages=['paymaster', 'paymaster.migrations', 'paymaster.templates'],
-    package_data={'paymaster': ['templates/paymaster/*.html']},
+    include_package_data=True,
 
     url='https://github.com/scailer/django-paymaster/',
     license = 'MIT license',

21424579053cecda4a9768f736bf3971cf4a0dea
Vlasov Dmitriy <scailer@veles.biz>
2014-02-24 16:17:39 +0600

    docs

--- paymaster/views.py
+++ paymaster/views.py
@@ -3,12 +3,13 @@
 import base64
 import urllib
 import hashlib
-
 from uuid import uuid4
 from datetime import datetime, timedelta
+
 from django.views import generic
 from django.http import HttpResponseRedirect, HttpResponse, QueryDict
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 
 from .models import Invoice
 from . import settings
@@ -17,6 +18,14 @@ from . import logger
 
 
 class InitialView(generic.FormView):
+    """
+        Форма инициации платежа.
+
+        Данный обработчик предназначен для формирования запроса инициации
+        платежа. Здесь будут сформированы параметры платежа в соответствии
+        с настройками приложения и имеющимися данными пользователя.
+    """
+
     _payment_no = None
 
     phone_field = 'phone'
@@ -24,14 +33,17 @@ class InitialView(generic.FormView):
     amount_key = 'amount'
 
     def form_valid(self, form):
+        """ Формируем переход на сайт платежной системы """
         if not self.request.user.is_authenticated():
             logger.warn(u'No user. Permission denied.')
             return HttpResponse('NO ACCESS', status=403)
 
-        url = '{0}?{1}'.format(
-            settings.PAYMASTER_INIT_URL,
-            self.init_query(form)
-        )
+        try:
+            url = '{0}?{1}'.format(
+                settings.PAYMASTER_INIT_URL,
+                self.init_query(form))
+        except ValidationError:
+            return self.form_invalid(form)
 
         logger.info(u'User {0} redirected to {1}'.format(
             self.request.user.username, url))
@@ -39,12 +51,15 @@ class InitialView(generic.FormView):
         return HttpResponseRedirect(url)
 
     def get_payer(self):
+        """ Получаем объект-плательщика """
         return self.request.user
 
     def get_amount(self, form):
+        """ Получаем сумму платежа """
         return form.data.get(self.amount_key)
 
     def get_paymant_no(self, form):
+        """ Генерируем номер платежа """
         if not self._payment_no:
             number = None
 
@@ -62,36 +77,44 @@ class InitialView(generic.FormView):
         return self._payment_no
 
     def get_description(self, form):
+        """ Получаем описание """
         return _(settings.PAYMASTER_DESCRIPTION_MASK).format(
             payer=self.get_payer(),
             number=self.get_paymant_no(form)
         )
 
     def get_description_base64(self, form):
+        """ Пререводим описание в base64 """
         return base64.encodestring(self.get_description(form).encode('utf-8'))
 
     def get_payer_phone(self, form):
+        """ Получаем номер телефона в формате 79031234567 """
         payer = self.get_payer()
         phone = getattr(payer, self.phone_field, None)
 
-        if phone:
-            return u''.join([x for x in phone if x in '1234567890'])
+        if phone is not None:
+            return u''.join(x for x in unicode(phone) if x in '1234567890')
 
     def get_payer_email(self, form):
+        """ Получаем электронную почту """
         payer = self.get_payer()
         return getattr(payer, self.email_field, None)
 
     def get_expires(self, form):
+        """ Получаем дату истечения счета YYYY-MM-DDThh:mm:ss """
         return (datetime.now() + timedelta(1)).strftime("%Y-%m-%dT%H:%M:%S")
 
     def get_payment_method(self, form):
+        """ Получаем идентификатор платежного метода """
         return settings.PAYMASTER_DEFAULT_PAYMENT_METHOD
 
     def get_extra_params(self, form):
+        """ Дополнительные параметры продавца """
         _d = isinstance(form.data, QueryDict) and form.data.dict() or form.data
         return _d
 
     def init_query(self, form):
+        """ Формируем параметры GET запроса """
         data = {
             'LMI_MERCHANT_ID': settings.PAYMASTER_MERCHANT_ID,
             'LMI_SHOP_ID': settings.PAYMASTER_SHOP_ID,
@@ -116,6 +139,9 @@ class InitialView(generic.FormView):
 
         data.update(self.get_extra_params(form))
 
+        # Сигнал посылается до создания счета (счет в дельнейшем может быть
+        # проигнорирован) с параметрами инициации платежа. Этот сигнал может
+        # использоваться для валидации данных (raise ValidationError).
         signals.invoice_init.send(sender=self, data=data)
 
         data = {k: v for k, v in data.items() if v}
@@ -123,17 +149,49 @@ class InitialView(generic.FormView):
 
 
 class ConfirmView(generic.View):
+    """
+        Обработчик предзапроса подтверждения счета (Invoice Confirmation).
+
+        Этот HTTP POST запрос отправляется системой PayMaster на веб-сервер
+        продавца, на URL, указанный в настройках, в тот момент, когда
+        пользователь выбрал платежную систему и собирается производить платеж.
+        Теоритически можно отказаться, однако, как указано в документации,
+        запрос пользователю на оплату уходит раньше, и следовательно возможна
+        ситуация, когда счет оплачен, но не принят продавцом. Чтобы избежать
+        лишних отказов, в данной реализации API отказ невозможен и счет будет
+        подтвержден в любом случае.
+
+        При получении этого запроса создается счет в БД продавца и отправлется
+        сигнал invoice_confirm c объектом-счетом в качестве параметра.
+    """
+
     def post(self, request):
+        # Создание счета в БД продавца
         invoice = Invoice.objects.create_from_api(request.POST)
         logger.info(u'Invoice {0} payment confirm.'.format(invoice.number))
+
+        # Отправка сигнал подтверждения счета.
         signals.invoice_confirm.send(sender=self, invoice=invoice)
         return HttpResponse('YES', content_type='text/plain')
 
 
 class NotificationView(generic.View):
+    """
+        Обработчик уведомления об оплате счета (Payment Notification).
+
+        HTTP POST запрос отправляется продавцу системой PayMaster в том случае,
+        когда платеж успешно проведен. Важно понимать, что запрос Payment
+        Notification - это единственный запрос, при обработке которого продавцу
+        необходимо учитывать принятый платеж (оказывать услугу и т.п.).
+
+        При получении этого запроса счет отмечается в БД как оплаченный и
+        отправлется сигнал invoice_paid c объектом-счетом в качестве параметра.
+    """
+
     _hash_fields = settings.PAYMASTER_HASH_FIELDS
 
     def check_hash(self, data):
+        """ Проверка ключа безопасности """
         _line = u';'.join([data.get(key) for key in self._hash_fields])
         _line += u';{0}'.format(settings.PAYMASTER_PASSWORD)
 
@@ -142,14 +200,15 @@ class NotificationView(generic.View):
         return _hash.hexdigest() == data.get('LMI_HASH')
 
     def post(self, request):
-        if not self.check_hash(request.POST):
+        if not self.check_hash(request.POST):  # Проверяем ключ
             logger.error(
                 u'Invoice {0} payment failed by reason: HashError'.format(
                     request.POST.get('LMI_PAYMENT_NO')))
             return HttpResponse('HashError')
 
         try:
-            invoice = Invoice.objects.finalize(request.POST)
+            invoice = Invoice.objects.finalize(request.POST)  # Закрываем счет
+
         except Invoice.InvoiceDuplication:
             logger.error(
                 u'Invoice {0} payment failed by reason: Duplication'.format(
@@ -159,11 +218,21 @@ class NotificationView(generic.View):
 
         logger.info(u'Invoice {0} paid succesfully.'.format(invoice.number))
 
+        # Отправляем сигнал об успешной оплате
         signals.invoice_paid.send(sender=self, invoice=invoice)
         return HttpResponse('')
 
 
 class SuccessView(generic.TemplateView):
+    """
+        Страница успешного возврата.
+
+        Предназначена исклчительно для уведомления пользователя об
+        удачном завершении операции.
+
+        Внимание! Этот запрос НЕ гарантирует оплаты.
+    """
+
     def get(self, request):
         invoice = Invoice.objects.get(pk=request.REQUEST['LMI_PAYMENT_NO'])
         logger.info(u'Invoice {0} success page visited'.format(invoice.number))
@@ -175,6 +244,13 @@ class SuccessView(generic.TemplateView):
 
 
 class FailView(generic.TemplateView):
+    """
+        Страница неуспешного возврата.
+
+        Предназначена исклчительно для уведомления пользователя об
+        неудачном завершении операции.
+    """
+
     def get(self, request):
         invoice = Invoice.objects.get(pk=request.REQUEST['LMI_PAYMENT_NO'])
         logger.info(u'Invoice {0} fail page visited'.format(invoice.number))

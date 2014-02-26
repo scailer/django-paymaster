22a429d98331a6826ea1730840240a9cac606f87
Vlasov Dmitriy <scailer@veles.biz>
2014-02-26 15:09:46 +0600

    fix auth_user_model

--- paymaster/models.py
+++ paymaster/models.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from .managers import InvoiceManager, InvoiceDuplication
-from . import settings
 
 
 class Invoice(models.Model):

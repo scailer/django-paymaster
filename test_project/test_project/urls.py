05e634a633e680cb9fac9a6f0327e306ed47fd34
aleksey <eremeev.dev@gmail.com>
2021-02-17 17:48:25 +0300

    fix url include

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('payments/', include('paymaster.urls', namespace='paymaster')),
+    path('payments/', include(('paymaster.urls', 'paymaster'), namespace='paymaster')),
 ]

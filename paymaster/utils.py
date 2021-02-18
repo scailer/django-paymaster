63ee49d168ca200d1cbefd82c976591a6603189a
aleksey <eremeev.dev@gmail.com>
2021-02-18 10:57:24 +0300

    don't encrypt user id

--- paymaster/utils.py
+++ paymaster/utils.py
@@ -3,24 +3,17 @@
 import base64
 from uuid import uuid4
 from datetime import datetime
-from simplecrypt import encrypt, decrypt, DecryptionException
 
 from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
 
-from . import settings
 from . import logger
 
 
 def decode_payer(enc):
     """ Декодирование пользователя-инициатора платежа """
     try:
-        secret = base64.decodestring(enc.encode('utf-8'))
-        pk = decrypt(settings.SECRET_KEY, secret)
-        return get_user_model().objects.get(pk=pk)
-
-    except DecryptionException:
-        logger.warn(u'Payer decryption error')
+        return get_user_model().objects.get(pk=enc)
 
     except get_user_model().DoesNotExist:
         logger.warn(u'Payer does not exist')
@@ -28,8 +21,7 @@ def decode_payer(enc):
 
 def encode_payer(user):
     """ Кодирование пользователя-инициатора платежа """
-    secret = encrypt(settings.SECRET_KEY, u"{}".format(user.pk))
-    return base64.encodestring(secret).decode('utf-8')
+    return str(user.pk)
 
 
 def number_generetor(view, form):
